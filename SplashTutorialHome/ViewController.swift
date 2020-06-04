9f83396fce00fd183ed4fe5f7e04c08a60b5c160
shoutokobayashi <deepstrap353@gmail.com>
2020-06-04 23:08:56 +0900

    Initial Commit

---
+++ SplashTutorialHome/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SplashTutorialHome
+//
+//  Created by 小林聖人 on 2020/06/04.
+//  Copyright © 2020 小林聖人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
